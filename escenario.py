1aedc6f4f9c5eacabcb9e8d166bf9643721a88b0
Alberto Lorente <lorentealberto@yahoo.es>
2018-04-26 11:23:25 +0200

    Cambio en la forma de cargar los recursos
    Implementado un nuevo sistema para cargar los recursos. Ahora se cargan directamente desde el archivo comprimido.

--- escenario.py
+++ escenario.py
@@ -1,3 +1,25 @@
+import pygame as py
+from utilidades import cargarImagen
+from configuracion import SIZE
+
+class Nube(object):
+	'''Clase nube, sirve para adornar el escenario'''
+	
+	'''Constructor'''
+	def __init__(self):
+		self.imagen = cargarImagen("nube", 4)
+		self.cuerpo = py.Rect(0, 0, self.imagen.get_width(), self.imagen.get_height())
+	
+	'''Dibuja los graficos en la pantalla
+		pantalla: superficie donde se dibujara'''
+	def dibujar(self, pantalla):
+		pantalla.blit(self.imagen, self.cuerpo)
+	
+	'''Actualiza el elemento'''
+	def actualizar(self):
+		self.cuerpo.move_ip(1, 0)
+		
+
 class Escenario(object):
     '''Clase para representar el escenario y todos sus elementos
         Fecha: 25/04/2018
@@ -5,15 +27,15 @@ class Escenario(object):
 
     '''Constructor: Inicializa todos los elementos del escenario'''
     def __init__(self):
-        pass
+        self.nube = Nube()
 
     '''Dibuja todos los elementos del escenario en la pantalla
         pantalla: superficie donde se dibujaran los elementos'''
     def dibujar(self, pantalla):
-        pass
+        self.nube.dibujar(pantalla)
 
     '''Actualiza todos los elementos del escenario
-        delta: tiempo de juego, necesario para algunas acciones'''
-    def actualizar(self, delta):
-        pass
+        delta: tiempo de juego, necesario para algunas acciones. opcional'''
+    def actualizar(self, delta = None):
+        self.nube.actualizar()
     
